8d284a9a83e5101aaf14cb4ad744b3369dd3856d
KodaCamp <>
2022-10-28 15:13:47 +0800

    comment edit & update

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,8 @@
 class CommentsController < ApplicationController
+  before_action :authenticate_user!, except: [:index]
   before_action :set_post
+  before_action :set_comment, only: [:edit, :update]
+
   def index
     @comments = @post.comments
   end
@@ -10,6 +13,7 @@ class CommentsController < ApplicationController
 
   def create
     @comment = @post.comments.build(params_comment)
+    @comment.user = current_user
     if @comment.save
       redirect_to post_comments_path(@post)
     else
@@ -17,10 +21,24 @@ class CommentsController < ApplicationController
     end
   end
 
+  def edit ;end
+
+  def update
+    if @comment.update(params_comment)
+      redirect_to post_comments_path
+    else
+      render :edit
+    end
+  end
+
   private
 
   def set_post
     @post = Post.find(params[:post_id])
+  end
+
+  def set_comment
+    @comment = @post.comments.find(params[:id])
   end
 
   def params_comment
