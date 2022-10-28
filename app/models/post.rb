59ed412872bcb76e3e002d1836d2a67324dd87fe
KodaCamp <>
2022-10-28 15:52:51 +0800

    rewrite destroy method

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,12 @@
 class Post < ApplicationRecord
+  default_scope{where(delete_at: nil)}
   validates :title, presence: true
   validates :content, presence: true
 
   belongs_to :user
   has_many :comments
+
+  def destroy
+    update(delete_at: Time.now)
+  end
 end
