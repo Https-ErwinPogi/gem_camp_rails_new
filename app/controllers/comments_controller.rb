13ed83fc4a7a9adcb989309cb4832ceec5ba5f38
KodaCamp <>
2022-10-28 16:14:20 +0800

    Validate comment user and hide the button and links

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,8 @@
 class CommentsController < ApplicationController
-  before_action :authenticate_user!, except: [:index]
+  before_action :authenticate_user!, except: :index
   before_action :set_post
   before_action :set_comment, only: [:edit, :update, :destroy]
+  before_action :validate_comment_owner, only: [:edit, :update, :destroy]
 
   def index
     @comments = @post.comments
@@ -48,5 +49,12 @@ class CommentsController < ApplicationController
 
   def params_comment
     params.require(:comment).permit(:content)
+  end
+
+  def validate_comment_owner
+    unless @comment.user == current_user
+      flash[:notice] = 'The comment not belongs to you'
+      redirect_to post_comments_path(@post)
+    end
   end
 end
