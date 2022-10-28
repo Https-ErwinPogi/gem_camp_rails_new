fe6030721cd2701d9049feef35d0b2080d574814
KodaCamp <>
2022-10-28 15:50:57 +0800

    add destroy comment action

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,7 @@
 class CommentsController < ApplicationController
   before_action :authenticate_user!, except: [:index]
   before_action :set_post
-  before_action :set_comment, only: [:edit, :update]
+  before_action :set_comment, only: [:edit, :update, :destroy]
 
   def index
     @comments = @post.comments
@@ -25,10 +25,15 @@ class CommentsController < ApplicationController
 
   def update
     if @comment.update(params_comment)
-      redirect_to post_comments_path
+      redirect_to post_comments_path(@post)
     else
       render :edit
     end
+  end
+
+  def destroy
+    @comment.destroy
+    redirect_to post_comments_path(@post)
   end
 
   private
