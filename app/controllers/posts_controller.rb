9805d71fb50a0e98062341476c6c0d76c4d89d48
KodaCamp <>
2022-10-28 12:09:17 +0800

    Refactor

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!, except: [:index]
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
   def index
     @posts = Post.includes(:user).all
   end
@@ -9,7 +10,7 @@ class PostsController < ApplicationController
   end
 
   def create
-    @post = Post.new(params[:post].permit( :title, :content))
+    @post = Post.new(post_params)
     @post.user = current_user
     if @post.save
       redirect_to posts_path
@@ -18,17 +19,12 @@ class PostsController < ApplicationController
     end
   end
 
-  def show
-    @post = Post.find(params[:id])
-  end
+  def show ;end
 
-  def edit
-    @post = Post.find(params[:id])
-  end
+  def edit ;end
 
   def update
-    @post = Post.find(params[:id])
-    if @post.update(params[:post].permit(:title, :content))
+    if @post.update(post_params)
       redirect_to posts_path
     else
       render :edit, status: :unprocessable_entity
@@ -36,8 +32,17 @@ class PostsController < ApplicationController
   end
 
   def destroy
-    @post = Post.find(params[:id])
     @post.destroy
     redirect_to posts_path
+  end
+
+  private
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+  def post_params
+    params[:post].permit(:title, :content)
   end
 end
